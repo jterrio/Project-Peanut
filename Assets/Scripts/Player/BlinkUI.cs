28153d73b0c2b06d9d8fc13f0c3063d974658b1c
Ryan DeBlanc <Ryan.deblanc@selu.edu>
2020-05-20 01:09:14 -0500

    Added Blinking
    -Press space to block vision
    -Added Basic UI with a panel for vision blocking that fades in and out.
    -Todo: Disable RayCasts for Player so Peanut can move.

---
+++ Assets/Scripts/Player/BlinkUI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkUI : MonoBehaviour
+{
+    public GameObject blinking;
+    public float blinkTime = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(Blinking(blinking.GetComponent<CanvasGroup>()));
+        }
+    }
+
+    /// <summary>
+    /// Disables Vision for Player as well as ToDo: blocks raycast so Peanut can move while blinked
+    /// </summary>
+    /// <param name="canvGroup"></param>
+    /// <returns></returns>
+    public IEnumerator Blinking(CanvasGroup canvGroup)
+    {
+        float counter = 0f;
+        float oneThirdBlink = blinkTime / 3;
+
+        //Closing Eyes
+        while (counter < oneThirdBlink) {
+            counter += Time.deltaTime;
+            canvGroup.alpha = Mathf.Lerp(0, 1, counter / oneThirdBlink);
+
+        }
+        //Time with eyes closed
+        //Todo: Turn off raycast for Player so Peanut can move
+        counter = 0f;
+        while (counter < oneThirdBlink) {
+            counter += Time.deltaTime;
+        }
+        //Todo: Turn on raycast for Player so Peanut can not move
+        //Opening Eyes
+        counter = 0f;
+        while (counter < oneThirdBlink) {
+            counter += Time.deltaTime;
+            canvGroup.alpha = Mathf.Lerp(1, 0, counter / oneThirdBlink);
+            yield return null; //Because we don't need a return value.
+        }
+    }
+}
